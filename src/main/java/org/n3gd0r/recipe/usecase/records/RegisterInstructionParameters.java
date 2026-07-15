5f877278ec7b962e260a23ad8675dedd4d2a557a
N3gD0r <70918021+N3gD0r@users.noreply.github.com>
2026-07-14 22:04:11 -0600

    Ensure not nulls on important stuff

--- src/main/java/org/n3gd0r/recipe/usecase/records/RegisterInstructionParameters.java
+++ src/main/java/org/n3gd0r/recipe/usecase/records/RegisterInstructionParameters.java
@@ -4,6 +4,7 @@ import org.springframework.util.Assert;
 
 public record RegisterInstructionParameters(Integer instructionNumber, String instruction) {
     public RegisterInstructionParameters {
+        Assert.notNull(instructionNumber, "The RegisterInstructionParameters instructionNumber should not be null");
         Assert.isTrue(instructionNumber > 0,
                 "The RegisterRecipeWithInstructionsParameters instructionNumber should not be less de 1");
         Assert.notNull(instructionNumber,
