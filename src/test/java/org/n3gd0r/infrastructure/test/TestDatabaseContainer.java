51b46709dfac386b8035f2cd21410529f30ff0b0
N3gD0r <70918021+N3gD0r@users.noreply.github.com>
2026-07-22 13:36:27 -0600

    Initial setup for test containers

---
+++ src/test/java/org/n3gd0r/infrastructure/test/TestDatabaseContainer.java
@@ -0,0 +1,7 @@
+package org.n3gd0r.infrastructure.test;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestDatabaseContainer {
+}
---
+++ src/test/java/org/n3gd0r/recipe/repository/RecipeRepositoryTest.java
@@ -0,0 +1,8 @@
+package org.n3gd0r.recipe.repository;
+
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+@DataJpaTest
+public class RecipeRepositoryTest {
+
+}
